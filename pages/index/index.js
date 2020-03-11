769ec8a1c0a7e9fe9a5b2c79f99ea36821bbe7c6
Thanakorn Ariyagusolsuthi <t.ariyagusolsuthi@gmail.com>
2020-03-11 15:58:14 +0700

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.h1`
+  color: red;
+`
+
+const Homepage = () => {
+  return <Header>Helloooooo</Header>
+}
+
+export default Homepage
